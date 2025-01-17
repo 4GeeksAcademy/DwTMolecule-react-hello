13666e17325737d6bad1fcd4d3cddcdd3ae9a219
MaTi <matiasnicolaaas@gmail.com>
2025-01-17 23:54:19 +0000

    bajada

---
+++ src/js/component/bajada.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Bajada = () => {
+    return (
+        <nav class="navbar bg-body-tertiary bg-dark" data-bs-theme="dark">
+           <div class="container-fluid">
+             <span class="navbar-text mx-auto">
+                Copyright Your Website 2019
+             </span>
+            </div>
+        </nav>
+        
+        
+    )
+};
+
+export default Bajada
